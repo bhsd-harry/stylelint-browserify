a60f0be5ef9aae75a36974b2b0acb51b2108c553
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-05-16 22:55:15 +0800

    Create bump.sh

---
+++ bump.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+npm run build && npm run lint
+if [[ $? -eq 0 ]]
+then
+	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+	git add -A
+	git commit -m "$1"
+	git push
+	git tag "$1"
+	git push origin "$1"
+	npm publish --tag "${2-latest}"
+fi
