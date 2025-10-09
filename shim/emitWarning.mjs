d50ded083e67ddbc9a211571322f27628e55b7f3
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-10-09 20:43:51 +0900

    chore: skip no-irregular-whitespace when testing ES8 bundle

--- shim/emitWarning.mjs
+++ shim/emitWarning.mjs
@@ -1,1 +1,3 @@
 export const emitDeprecationWarning = () => {};
+
+export const emitExperimentalWarning = () => {};
---
+++ shim/resolveFilePath.mjs
@@ -0,0 +1,4 @@
+/* eslint-disable jsdoc/require-jsdoc */
+export default function resolveFilePath(location) {
+	return location;
+}
